c9657054263d0ec11d0bf9aea38630cd9d32d11f
Piero - Goreu <pp.llerenalima@gmail.com>
2024-12-13 11:15:25 -0500

    Update Header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
